e5a341dfcbbc473e91d16a7cead8add32f750362
Asm <destroasm@gmail.com>
2023-04-11 19:09:10 +0300

    f

--- function.py
+++ function.py
@@ -2,24 +2,25 @@ from typing import Any, Iterable
 import re
 
 
-def filter_query(value: str, data: Iterable[str]):
+def filter_query(value: str, data: Iterable[str]) -> filter:
     return filter(lambda x: value in x, data)
 
-def unique_query(data: Iterable[str], *args: Any, **kwargs: Any):
+def unique_query(data: Iterable[str], *args: Any, **kwargs: Any) -> set:
     return set(data)
 
-def limit_query(value: str, data: Iterable[str]):
+def limit_query(value: str, data: Iterable[str]) -> list:
     limit: int = int(value)
     return list(data)[:limit]
 
-def map_query(value: str, data: Iterable[str]):
+def map_query(value: str, data: Iterable[str]) -> map:
     col_number = int(value)
     return map(lambda x: x.split(' ')[col_number], data)
 
-def sort_query(value: str, data: Iterable[str]):
+def sort_query(value: str, data: Iterable[str]) -> Iterable[str]:
     reverse = value == 'desc'
     return sorted(data, reverse=reverse)
 
-def regex(value: str, data: Iterable[str]):
+def regex(value: str, data: Iterable[str]) -> filter:
     reg = re.compile(value)
     return filter(lambda x: re.search(reg, x), data)
+
